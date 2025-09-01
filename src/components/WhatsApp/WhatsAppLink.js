5639a3f25e1ac44caf6db404a2e7282048f7ef6a
felipe bartolomeu <felipebart1999@gmail.com>
2025-09-01 18:45:16 -0300

    deletando alguns arquivos

--- src/components/WhatsApp/WhatsAppLink.js
+++ src/components/WhatsApp/WhatsAppLink.js
@@ -1,5 +1,5 @@
-const phoneNumber = "558488708809";
-const message = "Olá, gostaria de mais informações sobre seu atendimento.";
+const phoneNumber = "5581998602739";
+const message = "Olá, gostaria de mais informações sobre a academia";
 const encodedMessage = encodeURIComponent(message);
 
 export const whatsAppLink = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
